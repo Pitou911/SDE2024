81b3c7b17cfe50a910b6364a93e84b172164618e
Spookee <danielzekk@gmail.com>
2024-10-30 18:53:31 +0100

    mockito

--- carenest/src/test/java/com/example/carenest/carenest/controller/StudentControllerTest.java
+++ carenest/src/test/java/com/example/carenest/carenest/controller/StudentControllerTest.java
@@ -1,18 +1,7 @@
-package com.example.carenest.controller;
+package com.example.carenest.carenest.controller;
 
-import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.carenest.dto.HealthCaseRequest;
-import com.example.carenest.dto.HealthCaseResponse;
-import com.example.carenest.entity.HealthCase;
-import com.example.carenest.service.HealthCaseService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
